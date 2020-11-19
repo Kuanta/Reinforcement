f854584905f7d7bdfe825ef0a7bdaad5964f80da
Kuanta <erdemhan93@gmail.com>
2020-11-19 16:26:40 +0300

    Fixes sequential dataset

--- nn_filter.py
+++ nn_filter.py
@@ -1,5 +1,5 @@
 from DeepTorch import Trainer as trn
-from DeepTorch.Datasets.NumericalDataset import NumericalDataset
+from DeepTorch.Datasets.SequentialDataset import SequentialDataset
 import torch
 import torch.nn as nn
 from scipy.io import loadmat
@@ -7,9 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 TRAIN = True
+RESUME_TRAINING = False
 TEST = True
 NON_LINEAR = False
-MODEL_NAME = "./models/nn_filter"
+MODEL_NAME = "./models/nn/nn_filter"
 if NON_LINEAR:
     DATASET = "./data/nl_dataset.mat"
     MODEL_NAME = MODEL_NAME + "_nl"
@@ -37,25 +38,28 @@ class FilterNetwork(nn.Module):
 
 
 class LinearFilterNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, n_points):
         super(LinearFilterNetwork, self).__init__()
-        self.hidden_states = 7
+        self.hidden_states = 3
         self.hs = (torch.zeros(1, 1, self.hidden_states).to("cuda:0").float(),
                    torch.zeros(1, 1, self.hidden_states).to("cuda:0").float())
         self.lstm = nn.LSTM(7, self.hidden_states)
         self.fc2 = nn.Linear(self.hidden_states, 1)
+        self.fc3 = nn.Linear(7, 1)
+        self.n_points = n_points
 
     def forward(self, x):
-        x, hs = self.lstm(x.unsqueeze(dim=1), self.hs)
+        x, hs = self.lstm(x, self.hs)
         self.hs = (hs[0].detach(), hs[1].detach())
-        x = x.reshape(-1, self.hidden_states)
+        #x = x.reshape(-1, self.hidden_states)
         x = self.fc2(x)
+
         return x
 
     def reset(self):
         # Reset the states
-        self.hs = (torch.zeros(1, 1, self.hidden_states).to("cuda:0").float(),
-                   torch.zeros(1, 1, self.hidden_states).to("cuda:0").float())
+        self.hs = (torch.zeros(1, self.n_points, self.hidden_states).to("cuda:0").float(),
+                   torch.zeros(1, self.n_points, self.hidden_states).to("cuda:0").float())
 
 # Load data
 dataset = loadmat(DATASET)
@@ -75,16 +79,22 @@ test_reg = np.array([test["y2"], test["y2_1"], test["y2_2"], test["y2_3"], test[
 test_reg = np.transpose(test_reg).squeeze(0)
 test_labels = np.array(test["y1"])
 
-train_dataset = NumericalDataset(tr_reg, tr_labels, sequential=True)
-validation_dataset = NumericalDataset(val_reg, val_labels, sequential=True)
+train_dataset = SequentialDataset(tr_reg, tr_labels, n_points=100)
+
+validation_dataset = SequentialDataset(val_reg, val_labels, n_points=1)
+test_dataset = SequentialDataset(test_reg, test_labels)
+test_dataset.batch_index = 0
+
+validation_dataset = None
 
 # Load network
 if NON_LINEAR:
     net = FilterNetwork()
 else:
-    net = LinearFilterNetwork()
+    net = LinearFilterNetwork(n_points=100)
 
 net.to("cuda:0")
+net.reset()
 
 if TRAIN:
     trn_opts = trn.TrainingOptions()
@@ -103,28 +113,34 @@ if TRAIN:
     else:
 
         #These optiosn work best for linear case
-        trn_opts.batch_size = 5000
-        trn_opts.learning_rate = 0.01
+        trn_opts.batch_size = 10000
+        trn_opts.learning_rate = 0.005
         trn_opts.learning_rate_drop_type = trn.SchedulerType.StepLr
-        trn_opts.learning_rate_drop_factor = 0.9
+        trn_opts.learning_rate_drop_factor = 0.999
         trn_opts.learning_rate_drop_step_count = 500
         trn_opts.n_epochs = 1
-        trn_opts.l2_reg_constant = 0.000001
+        trn_opts.l2_reg_constant = 0
         trn_opts.saved_model_name = MODEL_NAME
         trn_opts.save_model = True
         trn_opts.shuffle_data = False
         trn_opts.optimizer_type = trn.OptimizerType.Adam
         trn_opts.epoch_reset_callback = net.reset
-
+        trn_opts.checkpoint_frequency = 500
 
     trainer = trn.Trainer(net, trn_opts)
+    if RESUME_TRAINING:
+        net.load_state_dict(torch.load(MODEL_NAME))
     trainer.train(torch.nn.MSELoss(), train_dataset, validation_set=validation_dataset)
     print("Done")
 
 if TEST:
     net.load_state_dict(torch.load(MODEL_NAME))
+    net.n_points = 1
+    net.reset()
     # Regular Test
-    test_preds = net.forward(torch.tensor(test_reg).to("cuda:0").float()).detach().cpu().numpy()
+    test_regs, test_labels = test_dataset.get_batch(-1, 0, device="cuda:0")
+    test_preds = net.forward(test_regs).detach().cpu().numpy()
+    test_preds = np.squeeze(np.squeeze(test_preds, axis=1), axis=1)
     mse = np.mean(np.square(np.array(test["y1"]) - np.array(test_preds)))
     plt.figure()
     plt.plot(np.array(test["y1"]))
@@ -140,13 +156,16 @@ if TEST:
     '''
     In this test, y2 inputs are fed into F(s) and the results are compared with y1
     '''
-    net.reset()
+    net.reset()  # Reset states
     test_inputs = np.transpose([test["y2"], test["y2_1"], test["y2_2"], test["y2_3"]]).squeeze(0)
     past_preds = [0, 0, 0]
     preds = []
     for i in range(len(test["tout"])):
+        # Input must be of rank 3
         curr_input = np.expand_dims(np.concatenate((test_inputs[i,:], np.array(past_preds)), axis=0), axis=0)
-        pred = net.forward(torch.tensor(curr_input).to("cuda:0").float()).item()
+        curr_input = np.expand_dims(curr_input, axis=0)
+        curr_input = torch.tensor(curr_input).to("cuda:0").float()
+        pred = net.forward(curr_input).item()
         preds.append(pred)
         past_preds.insert(0, pred)
         past_preds.pop(-1)
@@ -162,47 +181,47 @@ if TEST:
     plt.title("In place test 1 - MSE:{}".format(mse))
     plt.show()
 
-    # In place Test 2
-    '''
-    In this test, a step input is fed into the F(s)*T2(s) system and the results are compared to T1(s)
-    '''
-    net.reset()
-    diff_eq_coeffs = [0,   0.0828,   -0.0750,    1.5987,   -0.6065];
-    test_inputs = np.transpose([test["u"], test["u_1"], test["u_2"], test["u_3"]]).squeeze(0)
-    past_r2s = [0, 0, 0]
-    past_y2 = [0, 0]
-    control_signals = []
-    preds = []
-    for i in range(len(test["tout"])):
-        # F(s)
-        curr_input = np.expand_dims(np.concatenate((test_inputs[i,:], np.array(past_r2s)), axis=0),axis=0)
-        r2 = net.forward(torch.tensor(curr_input).to("cuda:0").float()).item()
-        control_signals.append(r2)
-        past_r2s.insert(0, r2)
-        past_r2s.pop(-1),
-
-        # T2(s)
-        curr_r2 = np.array([r2, past_r2s[0], past_r2s[1]])
-        curr_r2 = np.concatenate((curr_r2, np.array(past_y2)), axis=0)
-        if NON_LINEAR:
-            curr_r2 = np.sin(curr_r2)
-        _y2 = np.dot(curr_r2, diff_eq_coeffs)
-        past_y2.insert(0, _y2)
-        past_y2.pop(-1)
-        preds.append(_y2)
-
-    mse = np.mean(np.square(np.array(test["y1"]) - np.array(preds)))
-    plt.figure()
-    plt.plot(np.array(test["y1"]))
-    plt.legend("Y1")
-    plt.plot(np.array(preds))
-    plt.legend("Prediction")
-    plt.plot(np.array(test["y2"]))
-    plt.legend("Y2")
-    plt.title("In Place Test 2 - MSE:{}".format(mse))
-    plt.show()
-
-    plt.figure()
-    plt.plot(np.array(control_signals))
-    plt.title("Control Signal in In Place Test 2")
-    plt.show()
+    # # In place Test 2
+    # '''
+    # In this test, a step input is fed into the F(s)*T2(s) system and the results are compared to T1(s)
+    # '''
+    # net.reset()
+    # diff_eq_coeffs = [0,   0.0828,   -0.0750,    1.5987,   -0.6065];
+    # test_inputs = np.transpose([test["u"], test["u_1"], test["u_2"], test["u_3"]]).squeeze(0)
+    # past_r2s = [0, 0, 0]
+    # past_y2 = [0, 0]
+    # control_signals = []
+    # preds = []
+    # for i in range(len(test["tout"])):
+    #     # F(s)
+    #     curr_input = np.expand_dims(np.concatenate((test_inputs[i,:], np.array(past_r2s)), axis=0),axis=0)
+    #     r2 = net.forward(torch.tensor(curr_input).to("cuda:0").float()).item()
+    #     control_signals.append(r2)
+    #     past_r2s.insert(0, r2)
+    #     past_r2s.pop(-1),
+    #
+    #     # T2(s)
+    #     curr_r2 = np.array([r2, past_r2s[0], past_r2s[1]])
+    #     curr_r2 = np.concatenate((curr_r2, np.array(past_y2)), axis=0)
+    #     if NON_LINEAR:
+    #         curr_r2 = np.sin(curr_r2)
+    #     _y2 = np.dot(curr_r2, diff_eq_coeffs)
+    #     past_y2.insert(0, _y2)
+    #     past_y2.pop(-1)
+    #     preds.append(_y2)
+    #
+    # mse = np.mean(np.square(np.array(test["y1"]) - np.array(preds)))
+    # plt.figure()
+    # plt.plot(np.array(test["y1"]))
+    # plt.legend("Y1")
+    # plt.plot(np.array(preds))
+    # plt.legend("Prediction")
+    # plt.plot(np.array(test["y2"]))
+    # plt.legend("Y2")
+    # plt.title("In Place Test 2 - MSE:{}".format(mse))
+    # plt.show()
+    #
+    # plt.figure()
+    # plt.plot(np.array(control_signals))
+    # plt.title("Control Signal in In Place Test 2")
+    # plt.show()
